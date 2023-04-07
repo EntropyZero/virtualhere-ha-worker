d2befd9ea330758cc70e52c142105a2ae15ecd7c
Steven <steven.buxton@ent0.com>
2023-04-07 14:17:39 -0400

    SB -- Updating Error Message

--- src/prep.py
+++ src/prep.py
@@ -7,6 +7,7 @@ USB_DEVICE_ID = os.getenv('VH_USB_DEVICE_ID')
 print('Prepping VHClient with parameters:')
 if not all((HOSTNAME,USB_DEVICE_ID)):
    print("No Configuration provided.  Both Hostname and USB Device ID required.")
+   print("Device ID = {Hub Serial Number}.{USB Product ID}.{USB Vendor ID}.{Port Address (Optional)}")
    exit()
 
 print(f'Hostname: {HOSTNAME}')
